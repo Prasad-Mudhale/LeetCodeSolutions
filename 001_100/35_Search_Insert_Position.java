1f2da97a957bd22296d085049622700e61f0bafc
Prasad Mudhale <prasadmudhale7@gmail.com>
2026-07-04 08:35:49 +0530

    Search Insert Position Solution

--- 001_100/35_Search_Insert_Position.java
+++ 001_100/35_Search_Insert_Position.java
@@ -1,5 +1,29 @@
 package 001_100;
 
 public class 35_Search_Insert_Position {
-    
+
+    public int searchInsert(int[] nums, int target) {
+       int n = nums.length;
+        int left = 0;
+        int right = n-1;
+
+        int mid = 0;
+
+        for(int i=0;i<n;i++){
+
+            mid = (left+right)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+               left = mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+
+        return left;
+        
+    }
 }
