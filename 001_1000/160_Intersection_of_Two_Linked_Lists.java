e8e12c3a119c59470e7c9ab8cd8e6dda5d9dd38b
Prasad Mudhale <prasadmudhale7@gmail.com>
2026-07-06 16:18:25 +0530

    New linkedlist programs updated

---
+++ 001_1000/160_Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class 160_Intersection_of_Two_Linked_Lists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode h1 = headA;
+        ListNode h2 = headB;
+
+        while(h1!=h2){
+            
+
+            if(h1 == null){
+                h1 = headB;
+            }else{
+                h1 = h1.next;
+            }
+             if(h2 == null){
+                
+                h2 = headA;
+            }else{
+                h2 = h2.next;
+            }
+
+
+        }
+        return h1;
+    }
+}
