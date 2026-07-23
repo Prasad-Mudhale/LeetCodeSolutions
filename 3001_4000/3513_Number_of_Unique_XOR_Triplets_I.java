e3770cd187ace45c2645d766f13257861e9490ab
Prasad Mudhale <prasadmudhale7@gmail.com>
2026-07-23 22:12:54 +0530

    3513 solution

---
+++ 3001_4000/3513_Number_of_Unique_XOR_Triplets_I.java
@@ -0,0 +1,12 @@
+public class 3513_Number_of_Unique_XOR_Triplets_I {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+
+        return Integer.highestOneBit(n) << 1;
+        
+    }
+}
